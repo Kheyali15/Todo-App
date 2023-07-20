dedbc01efd91da37f5779405661304a50c7b5ddd
Kheyali15 <kheyalidhara15@gmail.com>
2023-07-20 10:40:19 +0530

    committing header and modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState} from "react";
+import { Button } from "reactstrap";
+import CreateTask from "../Modal/CreateTask";
 
 const Header = () => {
+
+  const [modal, setModal] = useState("")
+
+  const toggle = () => {
+    setModal(!modal)
+  }
+
   return (
     <>
-        This is header
+      <div
+        className="header text-center"
+        style={{
+          height: "200px",
+          width: "100%",
+          backgroundColor: "#e6ffff",
+          paddingTop: "40px",
+        }}
+      >
+        <h3>Todo App</h3>
+        <Button color="primary" style={{ padding: "8px" }} onClick={() => setModal(true)}>
+          Create Task
+        </Button>
+      </div>
+      <CreateTask toggle={toggle} modal={modal}/>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
